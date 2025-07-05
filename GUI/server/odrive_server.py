f8c2eebd02f68a721721b54e63bd85b34715a426
Oopsan204 <117756826+Oopsan204@users.noreply.github.com>
2025-07-05 11:08:27 +0700

    sua dong 295 gui sever

--- GUI/server/odrive_server.py
+++ GUI/server/odrive_server.py
@@ -295,4 +295,4 @@ if __name__ == "__main__":
     log = fibre.Logger(verbose=False)
     shutdown = fibre.Event()
 
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
